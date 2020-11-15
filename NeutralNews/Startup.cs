8a8b5c8e1030481764cb1a05016e403c2dbdccef
Nicole Creech <creechsn@mail.uc.edu>
2020-11-15 12:10:06 -0500

    Added code for local database connection

--- NeutralNews/Startup.cs
+++ NeutralNews/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,7 +25,11 @@ namespace NeutralNews
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.Configure<CookiePolicyOptions>(options =>
+
+            services.AddDbContext<LocalNewsDatabase>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+
+            services.Configure<CookiePolicyOptions>(options =>
       {
               // This lambda determines whether user consent for non-essential cookies is needed for a given request.
               options.CheckConsentNeeded = context => true;
